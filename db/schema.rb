accf9b5a7ddf9f3458552706bfed5a5cc50e91a1
Armando Fox <fox@cs.berkeley.edu>
2017-02-15 13:35:19 -0800

    added bootstrap-saas, rspec, cuke, and other basic scaffolding

---
+++ db/schema.rb
@@ -0,0 +1,48 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20170213212619) do
+
+  create_table "apps", force: :cascade do |t|
+    t.string "description"
+    t.string "deployment_url"
+    t.string "repository_url"
+  end
+
+  create_table "engagements", force: :cascade do |t|
+    t.integer  "organization_id"
+    t.integer  "app_id"
+    t.string   "category"
+    t.datetime "start_date"
+    t.string   "screencast_url"
+    t.string   "poster_preview_url"
+    t.string   "poster_url"
+    t.string   "presentation_url"
+    t.string   "prototype_deployment_url"
+    t.string   "student_names"
+  end
+
+  add_index "engagements", ["app_id"], name: "index_engagements_on_app_id"
+  add_index "engagements", ["organization_id"], name: "index_engagements_on_organization_id"
+
+  create_table "organizations", force: :cascade do |t|
+    t.string "name"
+    t.text   "description"
+    t.string "url"
+    t.string "contact_name"
+    t.string "contact_email"
+    t.string "primary_phone"
+    t.string "secondary_phone"
+  end
+
+end
