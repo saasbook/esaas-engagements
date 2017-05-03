44011c535f9efaf54c9209872175db05e1e32842
Armando Fox <fox@cs.berkeley.edu>
2017-05-03 10:17:47 -0700

    allow unauthenticated access to read-only actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-
-  http_basic_authenticate_with(:name => Figaro.env.auth_user!, :password => Figaro.env.auth_pass!) if Rails.env.production?
+  http_basic_authenticate_with(:name => Figaro.env.auth_user!, :password => Figaro.env.auth_pass!, :only => [:create,:update,:destroy]) if Rails.env.production?
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
