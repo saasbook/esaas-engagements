fdcd1b8ff7ff30366bbd8bf8b53a7d01e801484b
Andrew Halle <ahalle@berkeley.edu>
2017-09-25 09:21:39 -0700

    fix: featured scope now refers to app statuses by name

--- app/models/app.rb
+++ app/models/app.rb
@@ -9,7 +9,7 @@ class App < ActiveRecord::Base
   default_scope { order(:name => :asc) }
   enum :status => { :dead => 0, :development => 1, :in_use => 2, :in_use_and_wants_improvement => 3, :inactive_but_wants_improvement => 4, :pending => 5}
 
-  scope :featured, -> { where.not("status = 0 or status = 1") }
+  scope :featured, -> { where.not("status = ? or status = ?", App.statuses[:dead], App.statuses[:pending]) }
 
   def as_json(options={})
     options[:only] = [:id,:name,:description,:deployment_url,:repository_url]
