adc8c147b8292f09d756e662f2bd75d896a9c1a7
Julie Soohoo <jsx1219@berkeley.edu>
2017-10-27 22:00:43 +0000

    diff_users features

---
+++ features/step_definitions/diff_users_steps.rb
@@ -0,0 +1,22 @@
+Then /^I should see "(.*)" has type "(.*)" $/ do |username, type|
+    #text = :type.choose("")
+#   if page.respond_to? :should
+#     page.should have_content(text)
+#   else
+#     assert page.has_content?(text)
+#   end
+    pending
+  
+end
+
+Then /^I should see "(.*)" has SID "(.*)" $/ do |username, sid|
+    #text = type.choose("")
+#   if page.respond_to? :should
+#     page.should have_content(text)
+#   else
+#     assert page.has_content?(text)
+#   end
+    pending
+  
+end
+
