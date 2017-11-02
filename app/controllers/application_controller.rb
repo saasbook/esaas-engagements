c821a21bfe979dd4dba1e86b50e46c3fa8f39396
Kyriaki Papadimitriou <kyriakipapadimitriou@berkeley.edu>
2017-11-02 02:47:41 +0000

    inter 2.2 finished login redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,18 @@
 class ApplicationController < ActionController::Base
-
+  #, if: :storable_location?
   before_filter :logged_in?
-
+  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   private
-
+  @@name_path = nil
+  
   def logged_in?
+    @@name_path = request.env['PATH_INFO']
     redirect_to login_path and return unless
       (@current_user = User.find_by_id(session[:user_id])).kind_of?(User)
   end
+  
 end
