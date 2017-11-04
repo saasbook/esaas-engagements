a2b73138b3079584164e19fb8ab5c1de95df10da
Sungwon Cho <csungwon@berkeley.edu>
2017-11-03 20:09:43 -0700

    [quick] add coverage directory to .gitignore

--- spec/factories/iteration.rb
+++ spec/factories/iteration.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
 	factory :iteration do
 		customer_feedback "{\"duration\":\"45 min\",\"demeanor\":\"Strongly agree\",\"engaged\":\"Mostly agree\",\"engaged_text\":\"Everyone was engaged\",\"communication\":\"Strongly agree\",\"communication_text\":\"good\",\"understanding\":\"Strongly agree\",\"understanding_text\":\"nice\",\"effectiveness\":\"Strongly agree\",\"effectiveness_text\":\"believe in them\",\"satisfied\":\"Strongly agree\",\"satisfied_text\":\"Love the features\"}"
+		end_date "2017-03-23"
 	end
 end
