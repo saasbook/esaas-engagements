561a2fd6f57570f9172a034bf533f1bff7e966c8
csungwon <csungwon@berkeley.edu>
2017-11-12 19:52:54 -0800

    [config] SimpleCov and CodeClimate configurations (#33)
    * [config] SimpleCov and codeclimate settings changed
    
    add excluding patterns to SimpleCov configuration
    add excluding patterns to codeclimate configuration
    enforce all files to be loaded at the running of SimpleCov
    
    * [quick] fix configuration
    
    * [quick] delete codeclimate configs
    
    * [quick] re-add codeclimate config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,4 @@
+require 'simplecov'
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
@@ -7,8 +8,6 @@ require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
-require 'simplecov'
-SimpleCov.start
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
