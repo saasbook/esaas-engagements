e2333677307e4ca98f0c36d44012b40e7ff8aa38
Jianhui Li <omegali047@gmail.com>
2019-04-02 21:29:52 -0700

    Feature 2: adding vetting radio button for comments

--- app/models/app.rb
+++ app/models/app.rb
@@ -5,12 +5,12 @@ class App < ActiveRecord::Base
   has_many :engagements, dependent: :destroy
   has_many :iterations, :through => :engagements
 
-  validates_presence_of :name, :description, :org_id, :status, :vetting_status
+  validates_presence_of :name, :description, :org_id, :status
   validates_presence_of :repository_url, unless: :pending?
 
   enum status: [:dead, :development, :in_use, :in_use_and_wants_improvement, :inactive_but_wants_improvement, :pending]
   enum vetting_status: [:vetting_pending, :on_hold, :staff_approved,:customer_informed, :customer_confirmation_received, :declined_by_staff, :declined_by_customer, :declined_by_customer_available_next_sem, :back_up]
-  enum comment_type: [:contact_status, :app_functionality, :general]
+  enum comment_type: [:contact_status, :app_functionality, :general, :vetting]
 
   default_scope { order(:name => :asc) }
   scope :featured, -> { where.not("status = ? or status = ?", App.statuses[:dead], App.statuses[:pending]) }
