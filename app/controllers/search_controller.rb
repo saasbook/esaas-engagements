c7b68c506417efafa711110d0e2521658f212e1f
Sarah Lin <rhapsodylin@berkeley.edu>
2019-04-27 22:15:25 -0700

    iter2 feature5 search feature by ziyilin

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,41 +2,36 @@ class SearchController < ApplicationController
 
   def search
     keyword = params["keyword"]
-    @all_filters = ["Apps", "Organizations", "Users"]
-    filters = []
-    @all_filters.each do |filter|
-      if params[filter] == "1"
-        filters += [filter]
-      end
-    end
+    filters = params[:search_filters]
+    @all_filters = ["App", "Organization", "User"]
     if keyword.empty?
       flash[:notice] = "Please enter a keyword in the search box."
-      redirect_to results_path(:keyword => "", :filters => @all_filters) and return 
-    elsif filters.empty?
-      flash[:notice] = "Please choose at least one category."
-      redirect_to results_path(:keyword => "", :filters => []) and return 
+      redirect_to results_path(:keyword => "", :search_filters => @all_filters) and return 
+    elsif filters.nil?
+      flash[:notice] = "Please at least choose one category you want to search."
+      redirect_to results_path(:keyword => "", :search_filters => @all_filters) and return 
     end
-    flash[:notice] = "Search #{keyword} according to the filters #{filters.map{|x| x.inspect}.join(', ')}."
-    redirect_to results_path(:keyword => keyword, :filters => filters)
+    flash[:notice] = "Search #{keyword} according to the filters #{params[:search_filters].map{|x| x.inspect}.join(', ')}."
+    redirect_to results_path(:keyword => keyword, :search_filters => filters)
   end
 
   def results
     keyword = ("%" + params["keyword"] + "%").downcase
-    filters = params[:filters]
-    @apps = []
-    @orgs = []
-    @users = []
-    if filters.nil?
-      return
-    end
-    if filters.include?("Apps")
+    filters = params[:search_filters]
+    if filters.include?("App")
       @apps = App.where('lower(name) LIKE ?', keyword).all() | App.where('lower(description) LIKE ?', keyword).all()
+    else
+      @apps = []
     end
-    if filters.include?("Organizations")
+    if filters.include?("Organization")
       @orgs = Org.where('lower(name) LIKE ?', keyword).all()
+    else
+      @orgs = []
     end
-    if filters.include?("Users")
+    if filters.include?("User")
       @users = User.where('lower(name) LIKE ?', keyword).all()
+    else
+      @users = []
     end
   end
 end
