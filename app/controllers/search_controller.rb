0fddc87f07627a955edc6b5e589d35712917c612
Sarah Lin <rhapsodylin@berkeley.edu>
2019-04-27 23:55:35 -0700

    iter 2 feature 5 test accomplished by ziyilin

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,25 +2,33 @@ class SearchController < ApplicationController
 
   def search
     keyword = params["keyword"]
-    filters = params[:search_filters]
     @all_filters = ["App", "Organization", "User"]
+    filters = []
+    @all_filters.each do |filter|
+      if params[filter] == "1"
+        filters += [filter]
+      end
+    end
     if keyword.empty?
       flash[:notice] = "Please enter a keyword in the search box."
-      redirect_to results_path(:keyword => "", :search_filters => @all_filters) and return 
-    elsif filters.nil?
+      redirect_to results_path(:keyword => "", :filters => @all_filters) and return 
+    elsif filters.empty?
       flash[:notice] = "Please at least choose one category you want to search."
-      redirect_to results_path(:keyword => "", :search_filters => @all_filters) and return 
+      redirect_to results_path(:keyword => "", :filters => []) and return 
     end
-    flash[:notice] = "Search #{keyword} according to the filters #{params[:search_filters].map{|x| x.inspect}.join(', ')}."
-    redirect_to results_path(:keyword => keyword, :search_filters => filters)
+    flash[:notice] = "Search #{keyword} according to the filters #{filters.map{|x| x.inspect}.join(', ')}."
+    redirect_to results_path(:keyword => keyword, :filters => filters)
   end
 
   def results
     keyword = ("%" + params["keyword"] + "%").downcase
-    filters = params[:search_filters]
+    filters = params[:filters]
     @apps = []
     @orgs = []
     @users = []
+    if filters.nil?
+      return
+    end
     if filters.include?("App")
       @apps = App.where('lower(name) LIKE ?', keyword).all() | App.where('lower(description) LIKE ?', keyword).all()
     end
