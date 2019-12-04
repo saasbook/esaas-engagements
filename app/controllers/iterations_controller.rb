40c5b07169dde46d907cdcc2364be0c8da74526c
Anthony Shao <anthonyshao@berkeley.edu>
2019-12-03 21:04:06 -0800

    fixed minor issues

--- app/controllers/iterations_controller.rb
+++ app/controllers/iterations_controller.rb
@@ -96,7 +96,7 @@ class IterationsController < ApplicationController
   end
 
   def auth_user?
-    return current_user.coach? || current_user.client?
+    User.find_by_id(session[:user_id])&.coach? || User.find_by_id(session[:user_id])&.client?
   end
-  
+
 end
