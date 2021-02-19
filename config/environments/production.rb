d3a72da5fa4218d2eeec0233d0e66dbb00d10e42
Enjia Xu <enjiaxu@berkeley.edu>
2021-02-19 01:15:31 -0800

    changed all ENV sytax

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,10 +92,10 @@ Rails.application.configure do
     storage: :s3,
     s3_credentials: {
       bucket: ENV["S3_BUCKET_NAME"], # ENV.fetch('S3_BUCKET_NAME')
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
+      access_key_id: ENV["AWS_ACCESS_KEY_ID"], # ENV.fetch('AWS_ACCESS_KEY_ID')
+      secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"], # ENV.fetch('AWS_SECRET_ACCESS_KEY')
+      s3_region: ENV["AWS_REGION"], # ENV.fetch('AWS_REGION')
     },
-    s3_host_name: ENV.fetch('S3_HOST_NAME')
+    s3_host_name: ENV["S3_HOST_NAME"] #ENV.fetch('S3_HOST_NAME')
   }
 end
