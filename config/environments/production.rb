542cebe849076f0612a2639d88ae3a4474585893
Enjia Xu <enjiaxu@berkeley.edu>
2021-02-19 01:08:31 -0800

    test ENV change for S3 bucket name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      bucket: ENV["S3_BUCKET_NAME"], # ENV.fetch('S3_BUCKET_NAME')
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
