0f2083d2feb31a3904966cdce04750ebb6a4c9a7
Enjia Xu <enjiaxu@berkeley.edu>
2021-03-03 17:21:04 -0800

    fix

--- config/initializers/omniauth-github_raw_info.rb
+++ config/initializers/omniauth-github_raw_info.rb
@@ -1,6 +1,8 @@
 require 'omniauth-oauth2'
 
-OmniAuth::Strategies::GitHub.raw_info do
-  access_token.options[:mode] = :header
-  @raw_info ||= access_token.get('user').parsed
+OmniAuth::Strategies::GitHub.class_eval do
+  def raw_info
+    access_token.options[:mode] = :header
+    @raw_info ||= access_token.get('user').parsed
+  end
 end
