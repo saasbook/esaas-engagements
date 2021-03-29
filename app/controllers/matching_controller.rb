f6a55a994d06c2631b081fe158fa45a149f95372
Fred Zhang <zfred@berkeley.edu>
2021-03-29 15:24:42 -0700

    [feat] basic show ranking

--- app/controllers/matching_controller.rb
+++ app/controllers/matching_controller.rb
@@ -16,13 +16,12 @@ class MatchingController < ApplicationController
     @mockProjects = [["AFX Dance", "Create a website that allows admins of different levels in AFX Dance to organize their audition process and pick dancers.", 3],
                       ["BCal API Integration", "Unified portal for event requests and calendar management after transition from Oracle Calendar. ", 2],
                       ["CS61 series Lab assistant check-in", "Sign in portal for the 61 series lab assistants ", 3]]
-    
+    @currentPreference = Matching.find_or_create_by(:id => 1).preference
   end
 
   def progress
     @mockMatching = params[:matching_id]
     @mockStudents = [["Team 1", "Complete", 1], ["Team 2", "In Progress", 2], ["Team 3", "Complete", 3]]
-
   end
 
   def store
