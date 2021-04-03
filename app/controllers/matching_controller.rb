0345b7ce477be0fac67567cf1e1af17b6ec8c877
Fred Zhang <zfred@berkeley.edu>
2021-04-02 17:06:45 -0700

    [fix]: try heroku"

--- app/controllers/matching_controller.rb
+++ app/controllers/matching_controller.rb
@@ -19,6 +19,8 @@ class MatchingController < ApplicationController
                         "CS61 series Lab assistant check-in": "Sign in portal for the 61 series lab assistants"
                         }
     @currentPreference = Matching.find_or_create_by(:id => 1).preference
+    puts Matching.find_or_create_by(:id => 1) 
+    puts @currentPreference
   end
 
   def progress
