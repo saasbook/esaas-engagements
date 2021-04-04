4c4bd73e3db338fb7397324ba3de2a1feac67d42
Fred Zhang <zfred@berkeley.edu>
2021-04-03 18:04:00 -0700

    [fix] finish basic user types

---
+++ features/matching.feature
@@ -0,0 +1,9 @@
+Feature: Test matching feature 
+   As a student who is involved in the matching
+   I want to go to matching page
+   So that I can easily choose my preference for projects
+
+
+Scenario: I can log in as a student
+  Given I am logged in as "student" type
+  
--- features/step_definitions/auth_user_steps.rb
+++ features/step_definitions/auth_user_steps.rb
@@ -1,18 +1,27 @@
 When /^(?:|I )am logged in as "([^"]*)" type$/ do |type|
-  case type:
-  when "student"
-    user = Fred_test
-  when "client"
-
+  case type
+    when "coach"
+      user = YAML.load(File.read "#{Rails.root}/db/github_mock_login.yml")["test"] 
+    when "student"
+      user = YAML.load(File.read "#{Rails.root}/db/github_mock_login.yml")["test_student"]
+    when "client"
+      user = YAML.load(File.read "#{Rails.root}/db/github_mock_login.yml")["test_client"]
+    else 
+      raise Exception.new "Illegal user type!"
+  end
   OmniAuth.config.test_mode = true
   OmniAuth.config.add_mock(:github,
-                          {
-                            :uid => '12345',
-                            :info => {
-                                :name => user_type.name
-                                :nickname => user_type.github_uid
-                                :email => user_type.email
-                                :type_user => user_type
+                            {
+                              :uid => '6789',
+                              :info => {
+                                  :name => user['name'],
+                                  :nickname => user['github_uid'],
+                                  :email => user['email'],
+                                  # :type_user => user
+                              }
                             }
-                          })
+                          )
+  visit '/login'
+  click_link "Log in with GitHub"
+  visit "/apps"
 end
