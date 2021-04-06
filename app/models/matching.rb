0fa7c713926d6b7bcc7bdaf39f3eca0656953a9a
Enjia Xu <enjiaxu@berkeley.edu>
2021-04-06 00:47:15 -0700

    [feature] added some presence validation and description of match function

--- app/models/matching.rb
+++ app/models/matching.rb
@@ -3,4 +3,23 @@ class Matching < ActiveRecord::Base
     serialize :teams, Hash
     serialize :result, Hash
     serialize :projects, Array
+
+    validates_presence_of :name, :teams, :projects
+
+    # given :preferences exist, produces a matching between team_number and app_id, stores in :result
+    def match
+      # :preferences format:
+      # each team_number maps to an array of app_id's.
+      # position in array indicates ranking (position 0 is the most preferred project)
+      # number of keys is the number of teams participating
+      # num projects >= num teams
+      # example:
+      preferences = { "sp21-1" => [ "app_id1", "app_id2", "app_id3" ], "sp21-2" => [ "app_id3", "app_id2", "app_id1" ] }
+
+      # :result format:
+      # each team_number maps to one app_id
+      # match function updates the :result after running its matching algorithm
+      # example:
+      result = { "sp21-1" => "app_id1", "sp21-2" => "app_id3" }
+    end
 end
