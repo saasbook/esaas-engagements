f0590cb9925914c56f01b7adfd8c0eb6cdeed33d
Kevin Lo <klo824@berkeley.edu>
2021-04-13 13:29:25 -0700

    Matching algorithm completed. Added comments and logs for testing.

--- app/models/matching.rb
+++ app/models/matching.rb
@@ -4,13 +4,15 @@ class Matching < ActiveRecord::Base
   @@result = {}
   @@projects = []
 
+  /* Dummy data for testing in ruby console. Uncomment and run commands: m = Matching.create and m.match to see results
   @@projects =[1, 2, 3, 4, 5, 6, 7, 8]
   @@preferences = { 'sp21-1' => [1, 2, 3, 4, 5, 6, 7, 8], 'sp21-2' => [3, 2, 4, 1, 5, 7, 8, 6],
                   'sp21-3' => [2, 3, 4, 5, 1, 8, 7, 6], 'sp21-4' => [1, 2, 3, 4, 5, 6, 7, 8],
                   'sp21-5' => [5, 3, 2, 4, 6, 7, 8, 1], 'sp21-6' => [8, 7, 6, 5, 4, 3, 2, 1] }
   @@teams = { 'sp21-1' => [1, 2, 3, 4, 5, 6, 7, 8], 'sp21-2' => [3, 2, 4, 1, 5, 7, 8, 6],
                   'sp21-3' => [2, 3, 4, 5, 1, 8, 7, 6], 'sp21-4' => [1, 2, 3, 4, 5, 6, 7, 8],
-                  'sp21-5' => [5, 3, 2, 4, 6, 7, 8, 1], 'sp21-6' => [8, 7, 6, 5, 4, 3, 2, 1] }
+                  'sp21-5' => [5, 3, 2, 4, 6, 7, 8, 1], 'sp21-6' => [8, 7, 6, 5, 4, 3, 2, 1] } */
+
   # Global variables to be used across functions for convenience.
   $engagements = @@teams.keys
   $engagement_preferences = @@preferences
@@ -18,6 +20,7 @@ class Matching < ActiveRecord::Base
   $app_preferences = {}
   $matchings = {}
 
+    # Indexing and looking up projects (represented by ints) is confusing. Represent ints in arrays as strings.
     def convert_array_numbers_to_string(arr)
       return arr.map(&:to_s)
     end
@@ -32,56 +35,51 @@ class Matching < ActiveRecord::Base
       end
     end
 
-    # Check if any matchings are unstable
-    def rogue_couple_exists
-    end
-
-    # Engagment proposes to app. Update matching if app prefers new one more.
+    # Engagment proposes to apps. Update matching if app prefers new one more.
     # Return true for successful match, false otherwise.
-    def propose(app, engagement)
-      puts "Proposing: Team " + engagement + " to " + "App " + app
-      # if app already has matching
-      if $matchings.key? app
-        # compare rank of current match to new proposal
-        @previous_match = $matchings[app]
-        puts "Previous match: Team " + @previous_match
-        @app_preference = $app_preferences[app]
-        puts "App Preferences: Teams " + @app_preference.to_s
-        @rank_previous = @app_preference.index(@previous_match)
-        puts "Rank of previous match: " + @rank_previous.to_s
-        @rank_proposal = @app_preference.index(engagement)
-        puts "Rank of proposing: " + @rank_proposal.to_s
-        puts ""
-        # lower number is more preferred because earlier in the preference list
-        if @rank_proposal < @rank_previous
-          # update new match
-          $matchings[app] = engagement
-          puts "Matched: " + app + " to " + engagement
+    def propose(team, team_preferences)
+      team_preferences.each do |app|
+        puts "Team preferences: " + team_preferences.to_s
+        puts "Team " + team + " proposing to App " + app
+        # Check if the app is currently matched
+        if $matchings.key? app
+          # compare rank of current match to new proposing team
+          @previous_match = $matchings[app]
+          puts "Previous match: Team " + @previous_match
+          @app_preference = $app_preferences[app]
+          puts "App Preferences: Teams " + @app_preference.to_s
+          @rank_previous = @app_preference.index(@previous_match)
+          puts "Rank of previous match: " + @rank_previous.to_s
+          @rank_proposal = @app_preference.index(team)
+          puts "Rank of proposing: " + @rank_proposal.to_s
           puts ""
-          # old match must propose again starting from next highest preference
-          @engagement_preference = $engagement_preferences[@previous_match]
-          @engagement_preference.each do |app_to_propose|
-            @index_curr_app = @engagement_preference.index(app)
-            @index_propose_app = @engagement_preference.index(app_to_propose)
-            if @index_propose_app > @index_curr_app
-              if propose(app_to_propose, @previous_match)
-                puts "Previous team: " + @previous_match + "matched with app: " + app_to_propose
-                break
-              end
-            end
+
+          # lower number is more preferred because earlier in the preference list
+          if @rank_proposal < @rank_previous
+            # Update match
+            $matchings[app] = team
+            puts "Matched App " + app + " to Team " + team
+            puts ""
+            # Old match must propose again starting from next highest preference
+            @engagement_preference = $engagement_preferences[@previous_match]
+            puts "Previous Matched Team preferences: "
+            puts @engagement_preference.to_s
+            @index_of_match = @engagement_preference.index(app)
+
+            puts "Previous Team " + @previous_match + " proposing again"
+            propose(@previous_match, @engagement_preference.slice(@index_of_match + 1, @engagement_preference.length))
+            return true
           end
+          puts "App " + app + " prefers current matched Team " + @previous_match + " over proposing Team " + team
+          puts ""
+        else
+          # App has no current match. Set matching to current proposer.
+          $matchings[app] = team
+          puts "Matched App " + app + " to Team " + team
+          puts ""
           return true
         end
-
-        return false
-      else
-        # no current match exists for app  -> automatic match
-        $matchings[app] = engagement
-        puts "Matched: " + app + " to " + engagement
-        puts ""
-        return true
       end
-
     end
 
     # Match using Gale-Shapley
@@ -90,8 +88,10 @@ class Matching < ActiveRecord::Base
         return $matchings
       end
 
+      # Format the projects to be strings instead of ints for indexing/lookup convenience.
       $apps = convert_array_numbers_to_string($apps)
 
+      # Format the team preferences hash to have string values instead of ints representing projects.
       @engagement_preferences_formatted = {}
       $engagement_preferences.each do |key, value|
         value = convert_array_numbers_to_string(value)
@@ -101,17 +101,11 @@ class Matching < ActiveRecord::Base
 
       assign_apps_preferences
 
-      $engagements.each do |e|
-        puts "Team Preferences for team " + e + ": "
-        puts $engagement_preferences[e].to_s
-        puts ""
-        $engagement_preferences[e].each do |app|
-          if propose(app, e)
-            # matched, no need to propose again for now
-            break
-          end
-        end
+      # Each team will propose to the next preferred project (app).
+      $engagements.each do |team|
+          propose(team, $engagement_preferences[team])
       end
+
       puts $matchings
       return $matchings
     end
